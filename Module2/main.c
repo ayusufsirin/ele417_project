3e2639d6d49ea26c2cf849348e6d5b8e0ee9c392
Ömer Fatih Zünbül <63973327+omerfatihzunbul@users.noreply.github.com>
2023-01-04 16:06:11 +0300

    Module 2 and aes library added.
    Praise to be Allah, the most merciful

--- Module2/main.c
+++ Module2/main.c
@@ -1,12 +1,29 @@
-#include <msp430.h> 
+#include <msp430.h>
+#include <nrf24l01.h>
+#include <aes.h>
 
-
-/**
- * main.c
- */
 int main(void)
 {
-	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-	
-	return 0;
+    WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+
+    char *payload;
+    struct AES_ctx ctx;
+    uint8_t key[] = "PraiseBeToAllah";
+    uint8_t iv[] = "TheMostMerciful";
+
+    AES_init_ctx_iv(&ctx, key, iv);
+
+    nrfBeginRX();
+
+    while (1)
+    {
+        payload = nrfReceive();
+        if (nrfAvailable() > 0)
+        {
+            nrfBeginTX();
+            AES_CBC_decrypt_buffer(&ctx, payload, sizeof(payload));
+            nrfSend(payload);
+            nrfBeginRX();
+        }
+    }
 }
